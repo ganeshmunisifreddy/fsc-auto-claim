a83942269e45f06fef36229d368c15ad63b2e47f
Ganesh <ganeshmunisif@gmail.com>
2022-09-01 23:15:44 +0530

    fix: multiple claims

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 console.log("script injected...");
 
+var isClicked = false;
+
+var claimInterval = setInterval(() => {
+  claimQuestions();
+  console.log("called claim interval");
+}, 2000);
+
+// function clearFunc() {
+//   var buttons = document.getElementsByTagName("button");
+
+//   for (btn of buttons) {
+//     if (btn.value === "release") {
+//       if (claimInterval) {
+//         clearInterval(claimInterval);
+//       }
+//     }
+//   }
+// }
+
 function claimQuestions() {
+  //clearFunc();
   var buttons = document.getElementsByTagName("button");
 
   for (btn of buttons) {
     if (btn.value === "claim") {
-      //btn.click();
-      console.log("Claiming question...");
+      if (!isClicked) {
+        btn.click();
+        isClicked = true;
+        console.log("Claiming question...");
+      }
     }
   }
 }
-
-setInterval(() => {
-  //claimQuestions();
-  //console.log("called");
-}, 2000);
